33e49c94862dc55de921310cd22152d775a3eb8a
jackson <lordjackson@gmail.com>
2025-10-01 15:53:51 -0300

    adicionando menu no formato drawer

---
+++ site_super_shock/js/menu.js
@@ -0,0 +1,12 @@
+function mostrarMenu() {
+  const menuLateral = document.getElementById("menu-lateral");
+  const iconMenu = document.getElementById("img-menu");
+
+  menuLateral.classList.toggle("ativa");
+
+  if (menuLateral.classList.contains("ativa")) {
+    iconMenu.src = "img/icon-close-menu.png";
+  } else {
+    iconMenu.src = "img/icon-hamburger-menu.png";
+  }
+}
